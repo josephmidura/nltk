2826633b167216fa255a58e2662e639278085561
Joe Midura <josephmidura@utexas.edu>
2018-02-21 16:30:58 -0600

    first commit

---
+++ Porter.py
@@ -0,0 +1,20 @@
+# some practice with tokenizing words and sentences 
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+
+# first stem words
+example_words = ["python","pythoner","pythoning","pythoned","pythonly"]
+
+for w in example_words:
+    print(ps.stem(w)) #print the words
+
+# stem some sentences
+new_text = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
+
+words = word_tokenize(new_text)
+
+for w in words:
+    print(ps.stem(w)) #print the sentence
