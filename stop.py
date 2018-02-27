ae965e135c4979c1a0032b9c222201f4b56e5dc1
Joe Midura <josephmidura@utexas.edu>
2018-02-27 16:46:49 -0600

    misc updates

--- stop.py
+++ stop.py
@@ -1,13 +1,18 @@
+# stop is part of preprocessing
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 example_sent = "This is a sample sentence, showing off the stop words filtration."
 
-stop_words = set(stopwords.words('english'))
+stop_words = set(stopwords.words('english')) # use english
+
+# print(stop_words) # print the actual english stop words
 
 word_tokens = word_tokenize(example_sent)
 
-filtered_sentence = [w for w in word_tokens if not w in stop_words]
+# could use one line instead of for function
+#filtered_sentence = [w for w in word_tokens if not w in stop_words] 
 
 filtered_sentence = []
 
