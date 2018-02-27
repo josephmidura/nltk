e0147c98639c96a90292d7ac9ce6af3565761b31
Joe Midura <josephmidura@utexas.edu>
2018-02-27 16:56:30 -0600

    misc updates

--- Porter.py
+++ Porter.py
@@ -1,7 +1,9 @@
 # tokenizing words and sentences 
+# stemming is a form of data pre-processing
 
 from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize 
+# could it be faster if i use sent_tokenize?
 
 ps = PorterStemmer()
 
@@ -9,7 +11,7 @@ ps = PorterStemmer()
 example_words = ["python","pythoner","pythoning","pythoned","pythonly"]
 
 for w in example_words:
-    print(ps.stem(w)) #print the words
+    print(ps.stem(w)) #print the stem of the words
 
 # stem some sentences
 new_text = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
@@ -17,4 +19,4 @@ new_text = "It is important to by very pythonly while you are pythoning with pyt
 words = word_tokenize(new_text)
 
 for w in words:
-    print(ps.stem(w)) #print the sentence
+    print(ps.stem(w)) #print the stemmed sentence
