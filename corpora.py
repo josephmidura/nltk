0d1d53970e0bfc3e2d49f3cb9f80c4e574287014
Joe Midura <josephmidura@utexas.edu>
2018-03-14 11:31:27 -0500

    misc updates

---
+++ corpora.py
@@ -0,0 +1,13 @@
+# open gutenberg vible and read the first few lines
+# open gutenberg vible and read the first few lines
+
+from nltk.tokenize import sent_tokenize, PunktSentenceTokenizer
+from nltk.corpus import gutenberg
+
+# sample text
+sample = gutenberg.raw("bible-kjv.txt")
+
+tok = sent_tokenize(sample)
+
+for x in range(5):
+    print(tok[x])
