74a5cf9c72f13314a217aa10ba320853a9bb2df0
Joe Midura <josephmidura@utexas.edu>
2018-03-27 12:56:56 -0500

    misc updates

--- corpora.py
+++ corpora.py
@@ -1,4 +1,4 @@
-# open gutenberg vible and read the first few lines
+# open gutenberg bible and read the first few lines
 
 from nltk.tokenize import sent_tokenize, PunktSentenceTokenizer
 from nltk.corpus import gutenberg
