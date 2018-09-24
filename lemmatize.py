8a3ec370a56e4215869a6a4e3b1c43b8f199c20f
Joe Midura <josephmidura@utexas.edu>
2018-09-24 16:13:31 -0500

    misc updates

---
+++ lemmatize.py
@@ -0,0 +1,19 @@
+# examples of lemmatizing
+# show the room stem of workds
+# pos = part of speech (default is 'noun')
+# this isn't on github and I haven't watched the video
+
+
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+print(lemmatizer.lemmatize("cats"))
+print(lemmatizer.lemmatize("cacti"))
+print(lemmatizer.lemmatize("geese"))
+print(lemmatizer.lemmatize("rocks"))
+print(lemmatizer.lemmatize("python"))
+print(lemmatizer.lemmatize("better", pos="a"))
+print(lemmatizer.lemmatize("best", pos="a"))
+print(lemmatizer.lemmatize("run"))
+print(lemmatizer.lemmatize("run",'v'))
